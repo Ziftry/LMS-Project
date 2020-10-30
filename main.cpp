196a35f3ab22d078e923824747837a6f1a490578
ZIftry <73668403+ZIftry@users.noreply.github.com>
2020-10-30 18:05:33 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,23 +5,103 @@
 #include "book.h"
 #include "student.h"
 
-
 void loadBooks();
 void loadStudents();
+void printAllStudents();
+void login();
+void userInterface();
+void borrowBook();
+void returnBook();
 vector <book> books;
 vector <student> students;
+int studentIndex;
 
 int main() {
 	
 	cout << "Loading files from databases..." << endl << endl;
 	loadBooks();
 	loadStudents();
-	for (int i = 0; i < students.size(); i++) {
-		cout << "Student " << i << ":" << endl;
-		students[i].printStudent();
-		cout << endl;
-	}
+	studentIndex = -1;
+	userInterface();
+
+
 	cout << endl << "ALL DONE AND GOOD" << endl;
+}
+
+
+void borrowBook() {
+
+
+
+}
+
+void returnBook() {
+	int input;
+	cout << "What is the ID of the book you want to return?" << endl;
+	cin >> input;
+
+		
+
+
+
+
+
+
+
+}
+
+void userInterface() {
+	char input;
+	bool logout = false;
+	login();    //Calls the login() function which is user authentication
+	cout << "Welcome back, " << students[studentIndex].getUsername() << endl << endl;
+	while(!logout){
+		cout << "Please choose: " << endl;
+		cout << "        1 -- Borrow Books" << endl;
+		cout << "        2 -- Return Books" << endl;	
+		cout << "        0 -- Log Out" << endl;
+		cin >> input;
+	
+		switch (input) {
+		case '1':
+			borrowBook();
+			break;
+		case '2':
+			returnBook();
+			break;
+		case '0':
+			logout = true;
+			break;
+		default:
+			cout << "Not a correct input, please try again:";
+			break;
+		}
+	}
+}
+
+void login() {
+	while (1) {
+		string user = "", pass = "";
+		bool found = false;
+		cout << "Username:" << endl;
+		cin >> user;
+		cout << "Password:" << endl;
+		cin >> pass;
+
+		for (int i = 0; i <= students.size()-1 || found; i++) {
+			if (students[i].getUsername() == user && students[i].getPassword() == pass) {
+				found = true;
+				studentIndex = i;
+				break;
+			}
+		}
+		if (!found) {
+			cout << "No user with this password found" << endl << endl;
+		}
+		else {
+			break;
+		}
+	}
 }
 
 void loadBooks() {    //Reads the data from book.txt
@@ -64,4 +144,12 @@ void loadStudents() { //reads the data from students.txt
 		students.push_back(student(username, password, maxAllowed, maxTime, borrowed));
 	}
 	cout << "Successfully loaded student data" << endl << endl;
+}
+
+void printAllStudents() {
+	for (int i = 0; i < students.size(); i++) {
+		cout << "Student " << i << ":" << endl;
+		students[i].printStudent();
+		cout << endl;
+	}
 }
